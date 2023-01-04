a99144d415fd9b2e914c696e5889b73d84b6e893
Mariam1341 <mmm01061480913@gmail.com>
2023-01-04 16:49:26 +0200

    .

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -6,13 +6,6 @@ const navItem = document.getElementsByClassName("navbar__menu");
 const navList = document.getElementById("navbar__list");
 
 
-
-// Clicking on the button TOP will scroll to the top smoothly
-document.getElementById("top").addEventListener("click", () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-
-
 // when click on Add Section button it will create a new section and add it's link to the navbar 
 document.getElementById("btn").addEventListener("click", () => {
     createSec();
@@ -24,68 +17,95 @@ window.addEventListener('scroll', function () {
 
     //top button only visible when the user scrolls below the fold of the page.
     let currentScrollPos = window.pageYOffset;
-    // console.log(currentScrollPos)
+    //  
     if (currentScrollPos < 150) {
         document.getElementById("top").style.visibility = "hidden";
     } else {
         document.getElementById("top").style.visibility = "visible";
     }
-
-    //active state to the navigation items when a section is in the viewport
-    document.querySelectorAll("section").forEach((section) => {
-        const position = section.getBoundingClientRect();
-        const activeSec = document.getElementById("section" + section.id)
-        // checking whether fully visible
-        if (position.top >= 0 && position.bottom <= window.innerHeight) {
-            section.classList.add("your-active-class");
-            activeSec.classList.add("active-Section");
-        } else {
-            section.classList.remove("your-active-class");
-            activeSec.classList.remove("active-Section");
-        }
-
-    });
+    makeActive();
 });
 
+// make the shown section in the screen active to hover it
+function makeActive() {
+    document.querySelectorAll("section").forEach((section) => {
+        let activeSec = document.getElementById("section" + section.id);
+        let top = section.offsetTop - 400
+        let bottom = section.offsetHeight + section.offsetTop - 400;
+        window.addEventListener('scroll', () => {
+            let scrollPosition = window.scrollY;
+            if (scrollPosition > top && scrollPosition < bottom) {
+                section.classList.add("your-active-class");
+                activeSec.classList.add("active-Section");
+            } else {
+                section.classList.remove("your-active-class");
+                activeSec.classList.remove("active-Section");
+            }
+        });
+    });
+}
 
 // Global Functions
 
 //create new section
 function createSec() {
-    const section = `<section id="section${counter}" data-nav="Section ${counter}">
-    <div class="landing__container">
-    <h2>Section ${counter}</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
-    
-    <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
-    </div>
-    </section>`;
+    const section = document.createElement('section');
+    section.setAttribute('id', 'section' + counter);
+    section.setAttribute('data-nav', 'Section ' + counter);
+
+    const divSec = document.createElement('div');
+    divSec.setAttribute('class', 'landing__container');
+
+    const head = document.createElement('h2');
+    const htext = document.createTextNode(`Section ${counter}`);
+    head.appendChild(htext);
+
+    const pone = document.createElement('p');
+    const poneText = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.');
+    pone.appendChild(poneText);
+
+    const ptwo = document.createElement('p');
+    const ptwoText = document.createTextNode('Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.');
+    ptwo.appendChild(ptwoText);
+
+    divSec.appendChild(head);
+    divSec.appendChild(pone);
+    divSec.appendChild(ptwo);
+    section.append(divSec);
     counter++;
-    document.querySelector("main").innerHTML += section;
+    document.querySelector("main").appendChild(section);
 }
 
 
 //create Nav Bar Item
 function createNavbar() {
-    navList.innerHTML = " "  // we crete navbar from scratch when we add a section so we want to make sure that there is no data before we begain 
+    navList.innerHTML = " "
     document.querySelectorAll("section").forEach((section) => {
-        const liItem = `<li><a href="#${section.id}" data-nav="${section.id}" class="menu__link" id = "section${section.id}">
-        ${section.dataset.nav}</a></li>`;
-        navList.innerHTML += liItem;
+        const liItem = document.createElement('li');
+        const linkItem = document.createElement('a');
+        linkItem.setAttribute('class', 'menu__link');
+        linkItem.setAttribute('href', '#' + section.id);
+        linkItem.setAttribute('data-nav', section.id);
+        linkItem.setAttribute('id', 'section' + section.id);
+        const linkData = document.createTextNode(' ' + section.dataset.nav);
+        linkItem.appendChild(linkData);
+        liItem.appendChild(linkItem);
+        navList.appendChild(liItem);
     });
 }
 
 
-// we don't choose docment insted of navList becouse we need the funcnction apply only in the links sction 
-// and to prefent null value show when click on any place on the window 
-
-// smooth behaviour whn scroll to the target section
-navList.addEventListener("click", (event) => {
+// smooth scroll behaviour 
+document.addEventListener("click", (event) => {
     event.preventDefault();
     const id = event.target.dataset.nav;
-    if (id != null) {
-        document.getElementById(`${id}`).scrollIntoView({ behavior: "smooth" });
+    let toTop;
+    if (id === "top") {
+        toTop = 0;
+    } else if (id != null) {
+        toTop = document.getElementById(`${id}`).offsetTop;
     }
+    window.scrollTo({ top: toTop, behavior: 'smooth' });
 });
 
 // once the page load it will create navbar
